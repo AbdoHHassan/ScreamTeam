d4fcd72531515c66b7a4ed693f87413a040c4bbe
Lisa The <lisathe97@gmail.com>
2016-10-10 10:16:36 +0200

    Add clickable buttons
    - Show/Hide buttons depending if its an open question or not.

--- story.js
+++ story.js
@@ -4,6 +4,7 @@ var storyOutput;
 var answers = []; // Array of the questions + answers
 var answer;
 var storyCompleted = Boolean('false'); // turns to true once story is completed
+var openQuestion = Boolean('false'); //true when open question, false if not
 
 function init()
 {
@@ -13,11 +14,37 @@ updateStory();
 
 //Clears input field and replaces text
 function updateStory() {
-  var nextQuestion = storyOutput;
-  document.getElementById('answerField').value= "";
-  document.getElementById('story').innerHTML = nextQuestion;
-  document.getElementById('option1').firstChild.data ="Option One"; // Enter nextOption1 here
-  document.getElementById('option2').firstChild.data ="Option Two"; //Enter nextOption2 here
+    var nextQuestion = storyOutput;
+    document.getElementById('story').innerHTML = nextQuestion;
+    openQuestion=true;
+    
+    //HTML Elements. Wanted to move them to the top, but for some reason the boolean stops working. Not sure why..
+    var inputField = document.getElementById('answerField');
+    var optionOne = document.getElementById('option1');
+    var optionTwo = document.getElementById('option2');
+    var answerBtn = document.getElementById('submit');
+    
+    
+    //Hides or show the buttons depending on the question.
+    //TODO: Change the boolean somewhere. 
+    if (openQuestion == true)
+        {
+            optionOne.style.visibility="hidden";
+            optionTwo.style.visibility="hidden";
+            inputField.style.visibility="visible";  
+            answerBtn.style.visibility="visisble";
+            inputField.value = "";
+         
+        }
+    else if (openQuestion == false){
+        optionOne.style.visibility="visible";
+        optionTwo.style.visibility="visible";
+        inputField.style.visibility="hidden";
+        answerBtn.style.visibility="hidden";
+        
+        optionOne.firstChild.data ="Option One"; // Enter nextOption1 here
+        optionTwo.firstChild.data ="Option Two"; //Enter nextOption2 here
+        }
 };
 
 // There are currently three functions for input: One for text field, one for one button and another one for the second button..
@@ -40,30 +67,11 @@ function interpretAnswer() {
   checkAnswer();
 };
 
-function optionOne() {
-    alert("option one pressed");
-  //Retrieve question and answer
-  var question = document.getElementById('story').innerHTML;
-  answer = document.getElementById('option1').firstChild.data;
- 
-  //Creates an entry (q+a)
-  var entry = new Array();
-  entry.push({
-		question: question,
-        answer: answer
-   });
-    //Push array of (q+a)  into total q+a list
-    answers.push(entry);
-    alert(JSON.stringify(answers));
-    sendResults();
-  checkAnswer();
-};
+function interpretButtonAnswer(answeredElement) {
 
-function optionTwo() {
-    alert("option two pressed");
   //Retrieve question and answer
   var question = document.getElementById('story').innerHTML;
-  answer = document.getElementById('option2').firstChild.data;
+  answer = answeredElement.firstChild.data;
  
   //Creates an entry (q+a)
   var entry = new Array();
@@ -101,7 +109,7 @@ function checkAnswer() {
 
 function sendResults()
 {
-  //E-mail got banned while using some library for sending emails. Not again please :D
+  //E-mail got banned while using some library(EmailJS) for sending emails. Only other way is to use a download button, tho I am not sure if this is a good idea to put in the storyboard itself....?
     
 }
 
